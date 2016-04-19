348290e8380018ab2f68ffc16318591cf0cc1edd
Jørgen Lien Sellæg <jorgen@guut.org>
2016-04-19 23:49:47 +0200

    Snippet plugin now jumps around!

--- snippet.py
+++ snippet.py
@@ -1,4 +1,4 @@
-import jucipp, os, gi, traceback
+import jucipp, os, gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 from os import path
@@ -18,7 +18,7 @@ def add_menu(position, label, items) :
     if plugin_menu.get_n_items() >= position :
       plugin_menu.remove(position)
     plugin_menu.insert_submenu(position, label, sub_menu)
-    
+
 def init() :
   items = [
             {
@@ -30,9 +30,6 @@ def init() :
   ]
   add_menu(1, '_Snippet', items)
 
-def err(action, param) :
-  traceback.print_last()
-  
 def get_ifndef() :
   file_name = str(jucipp.editor.get_file_path())
   package, file_name = path.split(file_name)
@@ -47,31 +44,41 @@ def get_ifndef() :
 def get_snippet(word) :
   snippets = {}
 
-  snippets['for'] = '''\
-for (int i = 0; i < v.size(); i++) {
-  // std::cout << v[i] << std::endl;
-  // Write code here
-}\n'''
+  # iterate array
+  snippets['itar'] = '''\
+for (int i = 0; i < ${v.size()}; i++) {
+
+}'''
+  # iterate list
+  snippets['itli'] = '''\
+for (auto &${item:list_type}) {
+
+}'''
+
   snippets['if'] = '''\
-if(){
-  // Write code here
-}\n'''
+if (${}) {
+
+}'''
+
+  snippets['ife'] = '''\
+if (${}) {
+
+} else {
+
+}'''
 
   snippets['cout'] = '''\
-cout << \n'''
-  
+cout << ${} << "\\n";'''
   snippets['io'] = '''\
 #include <iostream>
-using namespace std;
-'''
+using namespace std;'''
   snippets['ifndef'] = get_ifndef()
-  
   try :
     output = snippets[word]
   except KeyError :
     output = word
   return output
-    
+
 def get_iter_at_cursor(gtk_text_buffer) :
   mark = gtk_text_buffer.get_insert()
   return gtk_text_buffer.get_iter_at_mark(mark)
@@ -101,13 +108,41 @@ def add_indention(text_line, output) :
     res += text_indent + text_line + '\n'
   return res[indent:len(res)]
 
+# jumps the marker to ${content} and marks the content
+def jump(start_iter, text) :
+  buf = start_iter.get_buffer()
+  start_pos = start_iter.get_offset()
+  end_pos = start_iter.get_offset()
+  pos = 0
+  stack = []
+  for c in text : # takes care of matching {}
+    if c is '$' : # constraint: comments with $ allowed in snippets
+      start_pos = start_pos + pos
+    if c is '{' :
+      stack.append(c)
+    if c is '}':
+      if stack.count(stack) is 0 :
+        end_pos = end_pos + pos
+        break
+      stack.pop()
+    pos = pos + 1
+  if pos is len(text) :
+    return
+  begin = buf.get_iter_at_offset(start_pos);
+  end = buf.get_iter_at_offset(end_pos+1);
+  token = buf.get_text(buf.get_iter_at_offset(start_pos+2),buf.get_iter_at_offset(end_pos),True)
+  buf.delete(begin,end)
+  buf.insert(begin,token,len(token))
+  buf.place_cursor(begin)
+  buf.move_mark(buf.get_insert(),buf.get_iter_at_offset(start_pos))
+
 def insert_snippet(action, param) :
   view = jucipp.editor.get_current_gtk_source_view()
   if not view :
     return
   gtk_text_buffer = view.get_buffer()
-  if not gtk_text_buffer : 
-    return  
+  if not gtk_text_buffer :
+    return
   input_line = get_current_line_text(gtk_text_buffer)
   if not input_line or input_line is '' :
     return
@@ -122,7 +157,11 @@ def insert_snippet(action, param) :
   line_number = get_iter_at_cursor(gtk_text_buffer).get_line()
   begin_iter = gtk_text_buffer.get_iter_at_line_offset(line_number,first_backwards_space)
   end_iter = gtk_text_buffer.get_iter_at_line_offset(line_number,line_offset)
-  gtk_text_buffer.delete(begin_iter, end_iter)
-  gtk_text_buffer.insert_at_cursor(snippet, len(snippet))
+  gtk_text_buffer.delete(begin_iter,end_iter)
+  gtk_text_buffer.insert_at_cursor(snippet, len(snippet)-1)
+  end_iter = get_iter_at_cursor(gtk_text_buffer)
+  end_iter.backward_chars(len(snippet)-1)
+  jump(end_iter, snippet)
+
 
 init()
